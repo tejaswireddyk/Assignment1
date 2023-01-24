3325298c26c8ebb41cf9a859c2e93161551dc809
tejaswireddyk <76957190+tejaswireddyk@users.noreply.github.com>
2023-01-23 19:09:38 -0600

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,6 @@
+import math
+radius = int(input("radius: "))
+
+_area_of_circle_ = math.pi*radius*radius
+_circum_of_circle_ = 2*math.pi*radius
+print("Area of circle=",_area_of_circle_, "\nCircumference of circle=",_circum_of_circle_)
