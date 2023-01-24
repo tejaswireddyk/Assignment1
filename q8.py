143b7b8d3096739454508fc5b234dbe735953bc8
tejaswireddyk <76957190+tejaswireddyk@users.noreply.github.com>
2023-01-23 19:11:55 -0600

    Create q8.py

---
+++ q8.py
@@ -0,0 +1,6 @@
+radius = 10
+print("Radius = {}".format(radius))
+
+print("area = {} * radius ** 2".format(3.14))
+
+print("The area of a circle with radius {} is {} meters square.".format(10,int(3.14* 10 **2)))
