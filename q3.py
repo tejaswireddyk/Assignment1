b6cf4cc8e7be52dfcaf093a24278a12981046331
tejaswireddyk <76957190+tejaswireddyk@users.noreply.github.com>
2023-01-23 19:08:41 -0600

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,5 @@
+brothers = ("Vamshi","Chandu","Mike")
+sisters = ("Akshaya","Priya","Donna","Rachel")
+siblings = brothers+sisters #Joining the tuples
+print(siblings)
+print("No. of siblings = ",len(siblings))
