5778e26cfdee16e3ce0ae0aa3ce069249233ff3d
tejaswireddyk <76957190+tejaswireddyk@users.noreply.github.com>
2023-01-24 19:35:45 -0600

    Delete q9.py

--- q9.py
+++
@@ -1,9 +0,0 @@
-def lbToKg(k):
-    return round(k*0.45359237,2) #converting lbs. to kilograms
-
-N = int(input())
-l  = list(map(int, input().split()))
-k=list()
-for i in range(N):
-    k.append(lbToKg(l[i]))
-print(k)
