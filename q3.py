5624be8a3158ed4734704118a06816ea37dcb398
tejaswireddyk <76957190+tejaswireddyk@users.noreply.github.com>
2023-01-24 19:39:37 -0600

    Delete q3.py

--- q3.py
+++
@@ -1,5 +0,0 @@
-brothers = ("Vamshi","Chandu","Mike")
-sisters = ("Akshaya","Priya","Donna","Rachel")
-siblings = brothers+sisters #Joining the tuples
-print(siblings)
-print("No. of siblings = ",len(siblings))
